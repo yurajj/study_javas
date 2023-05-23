0a74732c08324478aba730856157b0d9464ba897
yurajj <yul_j@naver.com>
2023-05-23 13:10:45 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String name = "jyr";
+        System.out.println(name);
+        // return 0;   고정적으로 해두기
+    }
+
+}
---
+++ src/Prints.java
@@ -0,0 +1,8 @@
+public class Prints {
+    
+    public static void main(String[] args) {
+        String name = "빅데이터 java 교육 과정";
+        System.out.println(name);
+    }
+
+}
