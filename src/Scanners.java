7fcd7102a8f90324353464ee372b67412bef87d7
yurajj <yul_j@naver.com>
2023-05-24 14:30:26 +0900

    3rd commit

---
+++ src/Scanners.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Scanners {
+
+    public static void main(String[] args) {
+
+        Scanner myObj = new Scanner(System.in); // 입력을 받는 준비하는 코드
+        // System.out.println("Enter username");
+
+        // String userName = myObj.nextLine(); // 실제로 입력을 받는 코드 
+        // System.out.println("Username is: " + userName); 
+        
+        int first = 0 ;
+        int second = 0 ;
+        first = myObj.nextInt();
+        second = myObj.nextInt();
+        System.out.println(first + second) ;
+
+        myObj.close();
+        // return 0;
+    }
+    
+}
