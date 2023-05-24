bbf64169b18e91d0d7e16bef60b307f0b6d2b04f
yurajj <yul_j@naver.com>
2023-05-24 13:14:23 +0900

    1000

---
+++ src/plusTwoVar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class plusTwoVar {
+
+    public static void main(String[] args) {
+
+        // 1000번 https://www.acmicpc.net/problem/1000
+        // 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+        Scanner sc = new Scanner(System.in) ;
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println(A+B) ;
+            sc.close();
+
+        
+        // return 0;
+    }
+    
+}
+
+    
+}
