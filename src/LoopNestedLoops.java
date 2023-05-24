c434db8084a653d14e509a0cb42e3ca9a05fccdd
yurajj <yul_j@naver.com>
2023-05-24 16:21:54 +0900

    5th commit

---
+++ src/LoopNestedLoops.java
@@ -0,0 +1,24 @@
+public class LoopNestedLoops {
+    
+
+    public static void main(String[] args) {
+
+
+        for(int first=1; first<5; first=first+1) {
+            System.out.println("for" + first) ;
+        
+            for(int second=1; second<=first; second=second+1) {
+                System.out.print("_?") ;
+            }
+        
+                System.out.println();
+
+        }
+    
+
+        System.out.println("End") ;
+
+        // return 0;
+    }
+}
+
