d4f85599cb582b400258db5844bdc71babff15fd
yurajj <yul_j@naver.com>
2023-05-24 18:22:59 +0900

    commit

---
+++ src/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+    
+
+    public static void main(String[] args) {
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        
+        for (int a=1; a<5; a=a+1) {
+            System.out.println(cars[a]);
+        }
+        
+        // return 0;
+    }
+}
