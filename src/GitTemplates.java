c8dde05f0bd49cf31d8c3e28dd4b8d6db80528a5
yurajj <yul_j@naver.com>
2023-05-31 11:08:47 +0900

    new gits

---
+++ src/GitTemplates.java
@@ -0,0 +1,14 @@
+public class GitTemplates {
+    
+    public static void main(String[] args) {
+        try {
+
+            int first = 0 ;
+            first = first + 1 ; 
+            
+        } catch (Exception e) {
+            // TODO : handle exception
+        }
+            //return 0;
+    } 
+}
