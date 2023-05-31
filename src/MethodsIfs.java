2d0b67a818c3990b3964673c537ddf90e2cfe2d5
yurajj <yul_j@naver.com>
2023-05-31 09:46:58 +0900

    commit

--- src/MethodsIfs.java
+++ src/MethodsIfs.java
@@ -11,7 +11,6 @@ public class MethodsIfs {
         try {
             // int time = 1; 
                 if (time<=12 && time>=1) {
-<<<<<<< HEAD
                     timeIs = "Morning." ;
                 }   
                 else if (time<=18 && time>=13) {
@@ -19,15 +18,6 @@ public class MethodsIfs {
                 }   
                 else if (time>=19 && time<=24) {
                     timeIs = "night." ;
-=======
-                    timeIs = "오전입니다." ;
-                }   
-                else if (time<=18 && time>=13) {
-                    timeIs = "오후입니다." ;
-                }   
-                else if (time <=19 && time >=24) {
-                    timeIs = "밤입니다." ;
->>>>>>> 7eecc6fb4e378520b0f405e3292b11c20d3e6b27
                 }
         } catch (Exception e) {
             // TODO : handle exception 
@@ -42,11 +32,7 @@ public class MethodsIfs {
             Scanner times = new Scanner(System.in);
 
             while (true) {
-<<<<<<< HEAD
                 System.out.println("time (1~24) : " ) ;
-=======
-                System.out.println("시간 입력 ( 1 ~ 24 ) : " ) ;
->>>>>>> 7eecc6fb4e378520b0f405e3292b11c20d3e6b27
                 int time = times.nextInt();
 
                 MethodsIfs methodsIfs = new MethodsIfs() ;
@@ -56,20 +42,12 @@ public class MethodsIfs {
                 // System.out.println("계속 하시겠습니까? (0:종료): ");
                 
 
-<<<<<<< HEAD
                 if (time == 0 || time >= 25)
                         System.out.println("Error. End.");
                         break ;
         
 
                 
-=======
-                if (time == 0 && time >= 25)
-                        System.out.println("잘못된 접근으로 종료합니다.");
-                        break ;
-        
-
->>>>>>> 7eecc6fb4e378520b0f405e3292b11c20d3e6b27
             }
             System.out.println() ;
             times.close();
