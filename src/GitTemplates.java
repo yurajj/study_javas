e14621bf6b0a753411fd3d91567ed5a6300fab0a
yurajj <yul_j@naver.com>
2023-05-31 11:09:54 +0900

    new github second

---
+++ src/GitTemplates.java
@@ -0,0 +1,16 @@
+public class GitTemplates {
+    
+    public static void main(String[] args) {
+        try {
+            
+            int first = 0 ;
+            int second = 1 ;
+
+            second = first + 1 ;
+
+        } catch (Exception e) {
+            // TODO : handle exception
+        }
+            // return 0;
+    }
+}
