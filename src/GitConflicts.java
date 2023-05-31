e7d5edd9467127a0be9647ad9dbdf2b39b9e3d29
yurajj <yul_j@naver.com>
2023-05-31 11:38:42 +0900

    first_B commit

---
+++ src/GitConflicts.java
@@ -0,0 +1,14 @@
+public class GitConflicts {
+    
+    public static void main(String[] args) {
+        try {
+
+            int first = 2 ;
+            first = first + 1 ; 
+            
+        } catch (Exception e) {
+            // TODO 
+        } 
+         // return 0;
+    }
+}
